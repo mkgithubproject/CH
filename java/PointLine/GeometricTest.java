e93f9b117ead679f7a421a6f51f3187ca5ca6f27
Mohit Kumar <37668348+mkgithubproject@users.noreply.github.com>
2022-03-13 21:44:27 +0530

    Create GeometricTest.java

---
+++ java/PointLine/GeometricTest.java
@@ -0,0 +1,8 @@
+public class GeometricTest {
+    public static void main(String[] args) {
+        Line obj=new Line(new Point(3,4),new Point(2,4)); // objet of line class which takes point class objects
+        System.out.println(obj.toString()); // print line instance
+    }
+
+
+}
