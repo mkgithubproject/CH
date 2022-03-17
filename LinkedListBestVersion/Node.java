fc99fad4b0598502db46d1f37d2c967182b66ad6
Mohit Kumar <37668348+mkgithubproject@users.noreply.github.com>
2022-03-17 12:51:06 +0530

    Create Node.java

---
+++ LinkedListBestVersion/Node.java
@@ -0,0 +1,24 @@
+public class Node<T>{
+    // instance variable
+    T data;
+    Node<T> next;
+    // constructor
+    public Node(T data) {
+        this.data = data;
+        this.next = null;
+    }
+    // getters and setters
+//    public T getData() {
+//        return data;
+//    }
+//    public Node<T> getNext() {
+//        return next;
+//    }
+//    public void setData(T data) {
+//        this.data = data;
+//    }
+//    public void setNext(Node<T> next) {
+//        this.next = next;
+//    }
+}
+
