94689c50ea44fd398a53d0aa94da9e900b15651f
Mohit Kumar <37668348+mkgithubproject@users.noreply.github.com>
2022-03-19 09:26:39 +0530

    Create javaScriptBasics.js

---
+++ basicJavaScript/javaScriptBasics.js
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="javascript.js">
+    </script>
+    <title>JavaScript Basics</title>
+</head>
+<body>
+    <div style="margin-top: 20px;">
+    <button type="button" onclick="average()">Average of 1 to 10</button>
+    <button type="button" onclick="if(isEven(6)===true){console.log('6 is even')}else{
+        console.log('6 is odd')
+    }">Test Number 6</button>
+    <button type="button" onclick="testNumber(245)">Test number 245</button>
+    <button type="button" onclick="dayActivity('Tuesday')">Day Activity Tuesday</button>
+    <button type="button" onclick="dayActivity('Sunday')">Day Activity Sunday</button>
+    <button type="button" onclick="printTable()">Print Table</button>
+</div>
+</body>
+</html>
