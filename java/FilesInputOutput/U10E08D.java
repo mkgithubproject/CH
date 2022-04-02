f5f85e2ab26d10c2fefd9dfc816f791914c254cf
Mohit Kumar <37668348+mkgithubproject@users.noreply.github.com>
2022-04-02 18:42:08 +0530

    Create U10E08D.java

---
+++ java/FilesInputOutput/U10E08D.java
@@ -0,0 +1,15 @@
+import java.io.IOException;
+import java.util.Scanner;
+public class U10E08D
+
+{
+
+    public static void main(String[] args) throws IOException
+
+    {
+        U10E08R r = new U10E08R();
+        r.testProgram();
+
+    }
+
+}
