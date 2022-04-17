541276f9cd62be69dd867dacc79b491cd73aa2f9
Mohit Kumar <37668348+mkgithubproject@users.noreply.github.com>
2022-04-17 19:15:42 +0530

    Create Pair.java

---
+++ java/ComparableInterface/Pair.java
@@ -0,0 +1,18 @@
+public class Pair <T extends Comparable<T>> {
+    private T item1;
+    private T item2;
+
+    public Pair(T i1, T i2) {
+        this.item1 = i1;
+        this.item2 = i2;
+    }
+    public T chooseItem(){
+        T chosenItem;
+        if(item1.compareTo(item2)<0){
+            chosenItem=item1;
+        }else{
+            chosenItem=item2;
+        }
+        return chosenItem;
+    }
+}
